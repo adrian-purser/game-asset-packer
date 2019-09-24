f975e9fb71cd9c93b3fbf1c4871888b1cacdd334
Adrian Purser <ade@arcadestuff.com>
2019-09-24 13:40:48 +0100

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+//=============================================================================
+//	FILE:					main.cpp
+//	SYSTEM:				Game Asset Packer
+//	DESCRIPTION:
+//-----------------------------------------------------------------------------
+//  COPYRIGHT:		(C)Copyright 2019 Adrian Purser. All Rights Reserved.
+//	LICENCE:
+//	MAINTAINER:		AJP - Adrian Purser <ade&arcadestuff.com>
+//	CREATED:			24-SEP-2019 Adrian Purser <ade&arcadestuff.com>
+//=============================================================================
+#include <iostream>
+
+
+int
+main(int argc,char ** argv)
+{
+	std::cout << "Hello Game Engine Packer World" << std::endl;
+
+
+	return 0;
+}
+
